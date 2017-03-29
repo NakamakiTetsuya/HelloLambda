19d853bc4950c4a640ca146921436fbf79fb7e08
NakamakiTetsuya <nakamaki.tetsuya@gmail.com>
2017-03-29 23:57:47 +0900

    twitter4j properties omit

--- src/main/java/example/Hello.java
+++ src/main/java/example/Hello.java
@@ -10,11 +10,19 @@ import twitter4j.QueryResult;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class Hello implements RequestHandler<String, String> {
 	public String handleRequest(String input, Context context) {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(System.getenv("twitter4j_debug").equals(true))
+				.setOAuthConsumerKey(System.getenv("twitter4j_oauth_consumerKey"))
+				.setOAuthConsumerSecret(System.getenv("twitter4j_oauth_consumerSecret"))
+				.setOAuthAccessToken(System.getenv("twitter4j_oauth_accessToken"))
+				.setOAuthAccessTokenSecret(System.getenv("twitter4j_oauth_accessTokenSecret"));
+		TwitterFactory tf = new TwitterFactory(cb.build());
 
-		Twitter twitter = new TwitterFactory().getInstance();
+		Twitter twitter = tf.getInstance();
 		Query query = new Query();
 		query.setLang("ja");
 		query.setCount(1);
@@ -28,7 +36,7 @@ public class Hello implements RequestHandler<String, String> {
 			return mapper.writeValueAsString(result.getTweets());
 		} catch (TwitterException e1) {
 			return null;
-		}  catch (JsonProcessingException e) {
+		} catch (JsonProcessingException e) {
 			return null;
 		}
 	}
